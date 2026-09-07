4ae2f8901bcc5d1c3117aa544c24a53331f2b1bf
Harsh Pandey <harshpa707@gmail.com>
2026-09-07 17:43:54 +0530

    DataBase connected

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv"
+
+import express from "express"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:"./env"
+})
+
+connectDB();
+
+const app = express();
+
+
